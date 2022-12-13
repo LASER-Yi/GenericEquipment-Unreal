61c1a73b395207925afa93cfbceb1bdf319e919f
LASER-Yi <liangyi0007@gmail.com>
2022-12-14 01:38:59 +0800

    Init repo

---
+++ Source/EquipmentCore/Private/EquipmentCore.cpp
@@ -0,0 +1,17 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EquipmentCore.h"
+
+#define LOCTEXT_NAMESPACE "FEquipmentCoreModule"
+
+void FEquipmentCoreModule::StartupModule()
+{
+}
+
+void FEquipmentCoreModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FEquipmentCoreModule, EquipmentCore)
